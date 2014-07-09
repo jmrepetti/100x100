1bd86493fc2181ceaa79785459621666572ebf45
jmrepetti <jmrepetti@gmail.com>
2014-07-10 01:49:47 +0200

    connect websocket to port 3001

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,5 @@
 var grid_id = window.location.pathname.split("/")[1]
-var socket = io.connect('/'+grid_id);
+var socket = io.connect(':3001/'+grid_id);
 
 socket.on('update', function (data) {
   document.getElementById(data.x + 'x' + data.y).style.backgroundColor="#"+data.rgb;
